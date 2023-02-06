77eefa98cce6b4ec986068875d3ad686899a2853
EL <ahmedgarnousy76@gmail.com>
2023-02-06 15:07:47 +0200

    changes readme

--- 3-Learn_JaveScript/1-JaveScript - The Complete Guide 2023(Beginner + Advanced)/07-DOM/dom-01-starting-code/app.js
+++ 3-Learn_JaveScript/1-JaveScript - The Complete Guide 2023(Beginner + Advanced)/07-DOM/dom-01-starting-code/app.js
@@ -1,14 +1,39 @@
-const h1 = document.querySelector("#main-title");
+const section = document.querySelector("section");
+const button = document.querySelector("button");
 
-h1.textContent = "some new title";
-h1.style.color = "white";
-h1.style.backgroundColor = "black";
+// section.style.backgroundColor = "blue";
+section.className = "red-bg ";
 
-// const listItemElements = document.querySelectorAll("li");
-const listItemElements = document.getElementsByTagName("li");
+const list = document.querySelector("ul");
 
-const body = document.body;
+button.addEventListener("click", () => {
+  // if (section.className === "red-bg visible") {
+  //   section.className = "red-bg invisible";
+  // } else {
+  //   section.className = "red-bg visible";
+  // }
 
-for (const listItemEl of listItemElements) {
-  console.dir(listItemEl);
-}
+  section.classList.toggle("visible");
+  section.classList.toggle("invisible");
+});
+
+// const ul = document.body.firstElementChild.nextElementSibling;
+// const firstLi = ul.firstElementChild;
+
+// const h1 = document.querySelector("#main-title");
+
+// h1.textContent = "some new title";
+// h1.style.color = "white";
+// h1.style.backgroundColor = "black";
+
+// const li = document.querySelector("li:last-of-type");
+// li.textContent = li.textContent + " (Changed!) ";
+
+// const body = document.body;
+
+// // const listItemElements = document.querySelectorAll("li");
+// const listItemElements = document.getElementsByTagName("li");
+
+// for (const listItemEl of listItemElements) {
+//   console.dir(listItemEl);
+// }
